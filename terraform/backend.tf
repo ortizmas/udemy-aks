9d1980ba58f921acf618654a310ecab9b62d47f2
Eber Ortiz <ortizmas14@gmail.com>
2026-08-23 21:38:26 -0300

    Add terraform

---
+++ terraform/backend.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.6"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.24"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "gh-terraform"
+    storage_account_name = "ghdevopsautomatf2" #colocar o seu nome para nao dar conflito
+    container_name       = "tfstate"
+    # key será definido via -backend-config no workflow
+  }
+}
